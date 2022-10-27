8eafd4240ed132e2fe0461f78fac971b1de7c0ed
Griffins Oringo <oringogriffins78@gmail.com>
2022-10-27 15:34:49 +0300

    fix

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+read -r -d '' DEPLOY <<EOF
+git pull
+chmod 777 -R storage/
+chmod 777 bootstrap/cache
+composer install --no-interaction
+php artisan migrate --force --step
+php artisan horizon:terminate
+php artisan view:clear
+rm -rf storage/logs/*.log
+EOF
+
+read -r -d '' COMMAND <<EOF
+cd /var/www/app
+$DEPLOY
+EOF
+
+git commit -a -m 'fix'
+git push && ssh root@loopy.co.ke "$COMMAND"
+
